6d62d5dbc4b7254d4fa9e5564df417a0f37983e0
leonier <utaujinkela@gmail.com>
2016-04-25 18:34:21 +0900

    Added tst.h for use.

---
+++ c/tst.h
@@ -0,0 +1,16 @@
+int parsewaveheader(unsigned char* wavhead, wavinfo* winfo);
+int finddatachunk(FILE* file);
+void writedatachunkhead(unsigned char* hdr, int wchannel, int wbits, int nsamples);
+void writepcmfmt(unsigned char* hdr, int wchannel, int wsrate, int wbits);
+void writeriffhead(unsigned char* hdr, int flength);
+
+void makedifferwav_lr(FILE* fpi, wavinfo* wav);
+void decodedifferwav_lr(FILE* fpi, wavinfo* wav);
+void decodedifferwav_32(FILE* fpi, wavinfo* wav);
+void makedifferwav_32(FILE* fpi, wavinfo* wav);
+void decodedifferwav_byte(FILE* fpi, wavinfo* wav);
+void makedifferwav_byte(FILE* fpi, wavinfo* wav);
+void compresswavtohalf(FILE* fpi, wavinfo* wav);
+void makehalfwav(FILE* fpi, wavinfo* wav);
+void decodedifferwav(FILE* fpi, wavinfo* wav);
+void makedifferwav(FILE* fpi, wavinfo* wav);
